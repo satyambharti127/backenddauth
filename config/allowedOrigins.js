a48edb04c0bceec627cebf102592a97757803ba7
Satyam <161875597+satyambharti127@users.noreply.github.com>
2025-06-24 23:54:59 +0530

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins=["https://www.google.com","https://localhost:5173","http://localhost:3010"]
+const allowedOrigins=["https://www.google.com","https://localhost:5173","http://localhost:3010","http://localhost:10000"]
 
 module.exports=allowedOrigins
